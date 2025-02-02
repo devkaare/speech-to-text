1e0d2554d53871943fdf47acfec7302323662e6f
devkaare <kaarehofferdal@gmail.com>
2025-02-02 22:59:36 +0100

    Feat

--- main.go
+++ main.go
@@ -5,29 +5,40 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/openai/openai-go"
 	"github.com/openai/openai-go/option"
 )
 
-const (
+var (
 	inputPath = "example-clips/english-corporate-meeting.wav"
 	// inputPath  = "example-clips/norwegian-topic-explanation.wav"
-	outputPath = "transcriptions/output.txt"
+	outputPath  = "transcriptions/output.txt" + time.Now().GoString()
+	summaryPath = "transcriptions/summary.txt" + time.Now().GoString()
 )
 
-func writeOutput(data string) error {
-	outputFile, err := os.OpenFile(outputPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+func writeToFile(filePath, data string) error {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		return err
 	}
-	defer outputFile.Close()
+	defer file.Close()
 
-	if _, err := outputFile.WriteString(data + "\n"); err != nil {
+	if _, err := file.WriteString(data + "\n"); err != nil {
 		return err
 	}
 	return nil
+}
+
+func readFromFile(filePath string) (string, error) {
+	rawData, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(rawData), nil
 }
 
 func main() {
@@ -53,22 +64,31 @@ func main() {
 		panic(err)
 	}
 
-	if err := writeOutput(audioResp.Text); err != nil {
+	if err := writeToFile(outputPath, audioResp.Text); err != nil {
 		panic(err)
 	}
 
 	fmt.Printf("[+] Successfully transcribed clip!\nFile: %s\nData: %s\n", inputPath, audioResp.Text)
 
+	data, err := readFromFile(outputPath)
+	if err != nil {
+		panic(err)
+	}
+
 	chatResp, err := client.Chat.Completions.New(
 		context.TODO(),
 		openai.ChatCompletionNewParams{
 			Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
-				openai.UserMessage("Create a short summary of this audio clip transcription: " + audioResp.Text),
+				openai.UserMessage("Create a short summary of this audio clip transcription: " + data),
 			}),
 			Model: openai.F(openai.ChatModelGPT4o),
 		},
 	)
 	if err != nil {
+		panic(err)
+	}
+
+	if err := writeToFile(summaryPath, chatResp.Choices[0].Message.Content); err != nil {
 		panic(err)
 	}
 
